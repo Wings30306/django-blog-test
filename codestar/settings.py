eecda05152ee27433139bf35ceab8ad990aa34bd
Joke Heyndels <jo_hannah@outlook.com>
2024-10-29 16:08:41 +0000

    Prepare for initial deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--p=xiz)bpcav%b6n)1chotxp0f*uulx*$rwy2qny9dwx21fwno'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-wings30306-djangoblogte-hinmo70lf3t.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-wings30306-djangoblogte-hinmo70lf3t.ws.codeinstitute-ide.net',
+                 '.herokuapp.com',]
 
 
 # Application definition
